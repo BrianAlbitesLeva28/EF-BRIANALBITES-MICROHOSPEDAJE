8fcd1b368cd303809ad9d0a0344fa8a72376df15
brian albites <sl73212959@idat.edu.pe>
2022-12-13 20:13:37 -0500

    gaaa

---
+++ src/main/java/com/idat/MicroservicioHospedaje/EfBrianAlbitesMicroservicioHospedajeApplication.java
@@ -0,0 +1,13 @@
+package com.idat.MicroservicioHospedaje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfBrianAlbitesMicroservicioHospedajeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EfBrianAlbitesMicroservicioHospedajeApplication.class, args);
+	}
+
+}
